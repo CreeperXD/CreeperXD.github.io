6e145f3fe2baf159dd47836f3baa275e78be5d2d
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 11:32:52 +0800

    Update ControlModel.js

--- mind-ar/ControlModel.js
+++ mind-ar/ControlModel.js
@@ -2,12 +2,14 @@ console.log("hello")
 let Models = document.getElementsByClassName("Model");
 
 setInterval(function() {
-    var Milliseconds = new Date().getMilliseconds();
-    Models.forEach(Model => {
-        if (Model.hasAttribute("rotation")) {
-            let NewValue = Math.sin(Milliseconds / 500 * Math.PI)
-            var NewRotation = "90 ".concat(NewValue, " 0");
-            Model.setAttribute("rotation", NewRotation);
-        }
-    });
+    Models.forEach(UpdateRotation);
 }, 20);
+
+function UpdateRotation(Model) {
+    var Milliseconds = new Date().getMilliseconds();
+    if (Model.hasAttribute("rotation")) {
+        let NewValue = Math.sin(Milliseconds / 500 * Math.PI)
+        var NewRotation = "90 ".concat(NewValue, " 0");
+        Model.setAttribute("rotation", NewRotation);
+    }
+}
