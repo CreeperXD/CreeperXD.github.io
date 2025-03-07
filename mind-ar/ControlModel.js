1a8fd11a7be938790f47886ed7374d8851e82cfc
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 11:57:14 +0800

    Revert "Update ControlModel.js"
    This reverts commit ae3c4d21f9afb015460f6b9e35a997705d72d90a.

--- mind-ar/ControlModel.js
+++ mind-ar/ControlModel.js
@@ -10,6 +10,7 @@ setInterval(function() {
         if (Model.hasAttribute("rotation")) {
             let NewValue = Math.sin(Milliseconds / 500 * Math.PI)
             var NewRotation = "90 ".concat(NewValue, " 0");
+            console.log(NewRotation)
             Model.setAttribute("rotation", NewRotation);
         }
     }
