9aeea43b1920c0e3af11243f27fdffbcffa8ed55
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 18:52:52 +0800

    change position

--- mind-ar/ModelsController.js
+++ mind-ar/ModelsController.js
@@ -6,8 +6,8 @@ setInterval(function() {
     for (let i = 0; i < Models.length; i++) {
         const Model = Models[i];
         if (Model.hasAttribute("position")) {
-            let NewValue = 1 + Math.sin(Milliseconds / 1000 * Math.PI) / 10;
-            var NewPosition = "0 0 ".concat(NewValue);
+            let NewValue = 0.5 + Math.sin(Milliseconds / 1000 * Math.PI) / 10;
+            var NewPosition = "0 ".concat(NewValue, " 0");
             console.log(NewPosition)
             Model.setAttribute("position", NewPosition);
         }
