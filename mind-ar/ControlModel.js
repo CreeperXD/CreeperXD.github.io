70f7a851665fca31be7c244cc04b0399d25ba17d
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 13:42:53 +0800

    2 models

--- mind-ar/ControlModel.js
+++ mind-ar/ControlModel.js
@@ -1,26 +1,18 @@
 console.log("hello");
 const Models = document.getElementsByClassName("Model");
 
+var Speed = 0.5
+
 setInterval(function() {
     var Milliseconds = new Date().getMilliseconds();
     console.log(Models.length)
-    //Models.forEach(UpdateRotation);
     for (let i = 0; i < Models.length; i++) {
         const Model = Models[i];
         if (Model.hasAttribute("rotation")) {
-            let NewValue = Math.sin(Milliseconds / 500 * Math.PI) * 180;
+            let NewValue = Math.sin(Milliseconds / 500 * Math.PI * Speed) * 180;
             var NewRotation = "90 ".concat(NewValue, " 0");
             console.log(NewRotation);
             Model.setAttribute("rotation", NewRotation);
         }
     }
 }, 20);
-
-// function UpdateRotation(Model) {
-//     var Milliseconds = new Date().getMilliseconds();
-//     if (Model.hasAttribute("rotation")) {
-//         let NewValue = Math.sin(Milliseconds / 500 * Math.PI)
-//         var NewRotation = "90 ".concat(NewValue, " 0");
-//         Model.setAttribute("rotation", NewRotation);
-//     }
-// }
