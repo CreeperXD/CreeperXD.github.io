2d398f9cfb4a4314373a71373c719645fd0c4135
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 17:39:23 +0800

    test rotation

--- mind-ar/ModelsController.js
+++ mind-ar/ModelsController.js
@@ -1,15 +1,13 @@
 console.log("hello");
 const Models = document.getElementsByClassName("CarModel");
 
-var Speed = 1
-
 setInterval(function() {
     var Milliseconds = new Date().getMilliseconds();
     for (let i = 0; i < Models.length; i++) {
         const Model = Models[i];
         if (Model.hasAttribute("rotation")) {
-            let NewValue = Milliseconds / 1000 * Speed * 360;
-            var NewRotation = "90 0 ".concat(NewValue);
+            let NewValue = Milliseconds / 1000 * 360;
+            var NewRotation = "90 ".concat(NewValue, " 0");
             console.log(NewRotation);
             Model.setAttribute("rotation", NewRotation);
         }
