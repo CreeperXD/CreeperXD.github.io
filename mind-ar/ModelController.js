6ce0c9a28c2448208a3d724f2d2b302f46e7138d
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 18:27:55 +0800

    Update ModelController.js

--- mind-ar/ModelController.js
+++ mind-ar/ModelController.js
@@ -5,10 +5,9 @@ setInterval(function() {
     var Milliseconds = new Date().getMilliseconds();
     for (let i = 0; i < Models.length; i++) {
         const Model = Models[i];
-        if (Model.hasAttribute("rotation")) {
-            let NewValue = Milliseconds / 1000 * 360;
-            var NewRotation = "0 0 ".concat(NewValue);
-            console.log(NewRotation);
+        if (Model.hasAttribute("position")) {
+            let NewValue = Milliseconds / 1000 * 100;
+            var NewPosition = "0 ".concat(NewValue, " ", NewValue);
             Model.setAttribute("rotation", NewRotation);
         }
     }
