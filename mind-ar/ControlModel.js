053b3122237286881ec5e2aab29af2e06487e380
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-07 16:27:17 +0800

    Update ControlModel.js

--- mind-ar/ControlModel.js
+++ mind-ar/ControlModel.js
@@ -5,11 +5,10 @@ var Speed = 1
 
 setInterval(function() {
     var Milliseconds = new Date().getMilliseconds();
-    console.log(Models.length)
     for (let i = 0; i < Models.length; i++) {
         const Model = Models[i];
         if (Model.hasAttribute("rotation")) {
-            let NewValue = Milliseconds % 1000 * Speed * 360;
+            let NewValue = (Milliseconds / 1000) % 1 * Speed * 360;
             var NewRotation = "90 0 ".concat(NewValue);
             console.log(NewRotation);
             Model.setAttribute("rotation", NewRotation);
