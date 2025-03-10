bc0af30586fde747f29aa1d3969fde835356a88e
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-10 14:58:35 +0800

    more models to visualise error

--- mind-ar/ModelController.js
+++ mind-ar/ModelController.js
@@ -1,8 +1,17 @@
 console.log("hello");
 
-const CarModel1s = document.getElementsByClassName("CarModel1");
-const CarModel2s = document.getElementsByClassName("CarModel2");
-const TimeScale = 0.5
+const CarModel1 = document.getElementsById("CarModel1");
+const CarModel2 = document.getElementsById("CarModel2");
+const CarModel3 = document.getElementsById("CarModel3");
+const CarModel4 = document.getElementsById("CarModel4");
+const CarModel5 = document.getElementsById("CarModel5");
+const CarModel6 = document.getElementsById("CarModel6");
+const CarModel7 = document.getElementsById("CarModel7");
+const CarModel8 = document.getElementsById("CarModel8");
+const CarModel9 = document.getElementsById("CarModel9");
+const CarModel10 = document.getElementsById("CarModel10");
+
+const TimeScale = 1
 
 var LastUpdatedSeconds = MillisecondsToSeconds(Date.now())
 var DeltaSeconds = 0
@@ -11,24 +20,56 @@ var CurrentTime = 0
 setInterval(UpdateTime, 0);
 
 setInterval(function() {
-    for (let i = 0; i < CarModel1s.length; i++) {
-        const Model = CarModel1s[i];
-        if (Model.hasAttribute("rotation")) {
-            let NewValue = GetFractionalPart(CurrentTime) * 360;
-            let NewRotation = "".concat(NewValue, " ", NewValue, " 0");
-            console.log(NewRotation)
-            Model.setAttribute("rotation", NewRotation);
-        }
+    let NewValue = GetFractionalPart(CurrentTime) * 360;
+    if (CarModel1.hasAttribute("rotation")) {
+        let NewRotation = "0 ".concat(NewValue, " 0");
+        console.log(NewRotation);
+        CarModel1.setAttribute("rotation", NewRotation);
     }
-
-    for (let i = 0; i < CarModel2s.length; i++) {
-        const Model = CarModel2s[i];
-        if (Model.hasAttribute("rotation")) {
-            let NewValue = GetFractionalPart(CurrentTime) * 360;
-            let NewRotation = "".concat(NewValue, " 0 ", NewValue);
-            console.log(NewRotation)
-            Model.setAttribute("rotation", NewRotation);
-        }
+    if (CarModel2.hasAttribute("rotation")) {
+        let NewRotation = "45 ".concat(NewValue, " 0");
+        console.log(NewRotation);
+        CarModel2.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel3.hasAttribute("rotation")) {
+        let NewRotation = "90 ".concat(NewValue, " 0");
+        console.log(NewRotation);
+        CarModel3.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel4.hasAttribute("rotation")) {
+        let NewRotation = "135 ".concat(NewValue, " 0");
+        console.log(NewRotation);
+        CarModel4.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel5.hasAttribute("rotation")) {
+        let NewRotation = "180 ".concat(NewValue, " 0");
+        console.log(NewRotation);
+        CarModel5.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel6.hasAttribute("rotation")) {
+        let NewRotation = "0 0 ".concat(NewValue);
+        console.log(NewRotation);
+        CarModel6.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel7.hasAttribute("rotation")) {
+        let NewRotation = "45 0 ".concat(NewValue);
+        console.log(NewRotation);
+        CarModel7.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel8.hasAttribute("rotation")) {
+        let NewRotation = "90 0 ".concat(NewValue);
+        console.log(NewRotation);
+        CarModel8.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel9.hasAttribute("rotation")) {
+        let NewRotation = "135 0 ".concat(NewValue);
+        console.log(NewRotation);
+        CarModel9.setAttribute("rotation", NewRotation);
+    }
+    if (CarModel10.hasAttribute("rotation")) {
+        let NewRotation = "180 0 ".concat(NewValue);
+        console.log(NewRotation);
+        CarModel10.setAttribute("rotation", NewRotation);
     }
 }, 0);
 
