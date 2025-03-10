9e2c6bfba230dfcb9487c9081d67242524511b30
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-10 12:55:10 +0800

    rotation and time

--- mind-ar/ModelController.js
+++ mind-ar/ModelController.js
@@ -1,25 +1,33 @@
 console.log("hello");
+
 const CarModel1s = document.getElementsByClassName("CarModel1");
 const CarModel2s = document.getElementsByClassName("CarModel2");
+const TimeScale = 1
+
+var DeltaTime = 0
+var CurrentTime = 0
 
 setInterval(function() {
-    var Milliseconds = new Date().getMilliseconds();
+    DeltaTime = Date.now() - DeltaTime;
+    CurrentTime += DeltaTime * TimeScale;
+
     for (let i = 0; i < CarModel1s.length; i++) {
         const Model = CarModel1s[i];
-        if (Model.hasAttribute("position")) {
-            let NewValue = 0.5 + Math.sin(Milliseconds / 1000 * Math.PI) / 10;
-            var NewPosition = "0 ".concat(NewValue, " 0");
-            console.log(NewPosition)
-            Model.setAttribute("position", NewPosition);
+        if (Model.hasAttribute("rotation")) {
+            let NewValue = CurrentTime % 1000 * 360;
+            var NewRotation = "90 ".concat(NewValue, " 0");
+            console.log(NewRotation)
+            Model.setAttribute("rotation", NewRotation);
         }
     }
+
     for (let i = 0; i < CarModel2s.length; i++) {
         const Model = CarModel2s[i];
-        if (Model.hasAttribute("position")) {
-            let NewValue = 0.5 + Math.sin(Milliseconds / 1000 * Math.PI) / 10;
-            var NewPosition = "0 0 ".concat(NewValue);
-            console.log(NewPosition)
-            Model.setAttribute("position", NewPosition);
+        if (Model.hasAttribute("rotation")) {
+            let NewValue = CurrentTime % 1000 * 360;
+            var NewRotation = "90 0 ".concat(NewValue);
+            console.log(NewRotation)
+            Model.setAttribute("rotation", NewRotation);
         }
     }
 }, 20);
