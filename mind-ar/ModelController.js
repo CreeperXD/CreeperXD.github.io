c648db6b50a2a8168ecc1f06197ca8e951d19f1a
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-10 14:28:35 +0800

    modify time changing

--- mind-ar/ModelController.js
+++ mind-ar/ModelController.js
@@ -4,19 +4,18 @@ const CarModel1s = document.getElementsByClassName("CarModel1");
 const CarModel2s = document.getElementsByClassName("CarModel2");
 const TimeScale = 0.5
 
-var DeltaTime = Date.now()
+var LastUpdatedSeconds = MillisecondsToSeconds(Date.now())
+var DeltaSeconds = 0
 var CurrentTime = 0
 
-setInterval(function() {
-    DeltaTime = Date.now() - DeltaTime;
-    CurrentTime += DeltaTime * TimeScale;
-    console.log(CurrentTime)
+setInterval(UpdateTime, 0);
 
+setInterval(function() {
     for (let i = 0; i < CarModel1s.length; i++) {
         const Model = CarModel1s[i];
         if (Model.hasAttribute("rotation")) {
-            let NewValue = (CurrentTime % 1000) / 1000 * 360;
-            var NewRotation = "".concat(NewValue, " ", NewValue, " 0");
+            let NewValue = GetFractionalPart(CurrentTime / 1000) * 360;
+            let NewRotation = "".concat(NewValue, " ", NewValue, " 0");
             //console.log(NewRotation)
             Model.setAttribute("rotation", NewRotation);
         }
@@ -25,10 +24,26 @@ setInterval(function() {
     for (let i = 0; i < CarModel2s.length; i++) {
         const Model = CarModel2s[i];
         if (Model.hasAttribute("rotation")) {
-            let NewValue = (CurrentTime % 1000) / 1000 * 360;
-            var NewRotation = "".concat(NewValue, " 0 ", NewValue);
+            let NewValue = GetFractionalPart(CurrentTime / 1000) * 360;
+            let NewRotation = "".concat(NewValue, " 0 ", NewValue);
             //console.log(NewRotation)
             Model.setAttribute("rotation", NewRotation);
         }
     }
-}, 20);
+}, 0);
+
+function UpdateTime() {
+    let CurrentSeconds = MillisecondsToSeconds(Date.now());
+    DeltaSeconds = CurrentSeconds - LastUpdatedSeconds;
+    LastUpdatedSeconds = CurrentSeconds;
+    CurrentTime += DeltaSeconds * TimeScale;
+    console.log(CurrentTime);
+}
+
+function MillisecondsToSeconds(Milliseconds) {
+    return Milliseconds / 1000;
+}
+
+function GetFractionalPart(Number) {
+    return Number % 1;
+}
