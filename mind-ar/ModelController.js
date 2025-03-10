c755bd799e64f2a3cfb357eb14b2e1d191275c89
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2025-03-10 11:49:19 +0800

    make the other marker move the car differently

--- mind-ar/ModelController.js
+++ mind-ar/ModelController.js
@@ -1,13 +1,23 @@
 console.log("hello");
-const Models = document.getElementsByClassName("CarModel");
+const CarModel1s = document.getElementsByClassName("CarModel1");
+const CarModel2s = document.getElementsByClassName("CarModel2");
 
 setInterval(function() {
     var Milliseconds = new Date().getMilliseconds();
-    for (let i = 0; i < Models.length; i++) {
-        const Model = Models[i];
+    for (let i = 0; i < CarModel1s.length; i++) {
+        const Model = CarModel1s[i];
         if (Model.hasAttribute("position")) {
             let NewValue = 0.5 + Math.sin(Milliseconds / 1000 * Math.PI) / 10;
             var NewPosition = "0 ".concat(NewValue, " 0");
+            console.log(NewPosition)
+            Model.setAttribute("position", NewPosition);
+        }
+    }
+    for (let i = 0; i < CarModel2s.length; i++) {
+        const Model = CarModel2s[i];
+        if (Model.hasAttribute("position")) {
+            let NewValue = 0.5 + Math.sin(Milliseconds / 1000 * Math.PI) / 10;
+            var NewPosition = "0 0 ".concat(NewValue);
             console.log(NewPosition)
             Model.setAttribute("position", NewPosition);
         }
