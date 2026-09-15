93edd5074f1ec4b722cc31068a4c4d03470db1f6
CreeperXD <xdtrollololxd@gmail.com>
2026-09-16 00:48:04 +0800

    listing out projects

--- ProjectsList.js
+++ ProjectsList.js
@@ -3,16 +3,157 @@ import Project, {ProjectType} from "./Project.js";
 
 
 const ProjectsList = [
-    new Project("game1", "test description1", "assets/images/EmailLogo.png", ProjectType.GAME, true, "#"),
-    new Project("notgame1", "test description2", "assets/images/EmailLogo.png", ProjectType.NOT_GAME, true, "#"),
-    new Project("game2", "test description3", "assets/images/EmailLogo.png", ProjectType.GAME, false, "#"),
+    new Project(
+        "laksa mana?",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/laksa mana?.html"
+    ),
+    new Project(
+        "Done Deals",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Done Deals.html"
+    ),
+    new Project(
+        "Time Mage",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Time Mage.html"
+    ),
+    new Project(
+        "Ministry of Spinning",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Ministry of Spinning.html"
+    ),
     new Project(
         "Shiplogging",
         "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
         "assets/images/Shiplogging/cover_image_630_500.png",
         ProjectType.GAME,
         true,
-        "./projects/Shiplogging.html"
+        "projects/Shiplogging.html"
+    ),
+    new Project(
+        "Godot stat bar",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.NOT_GAME,
+        true,
+        "projects/Godot stat bar.html"
+    ),
+    new Project(
+        "Godot 3D character controller",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.NOT_GAME,
+        true,
+        "projects/Godot 3D character controller.html"
+    ),
+    new Project(
+        "Preys in... what where is this?",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Preys in... what where is this?.html"
+    ),
+    new Project(
+        "Godot project template",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.NOT_GAME,
+        true,
+        "projects/Godot project template.html"
+    ),
+    new Project(
+        "Godot dialogue system",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.NOT_GAME,
+        true,
+        "projects/Godot dialogue system.html"
+    ),
+    new Project(
+        "Jiangshi",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Jiangshi.html"
+    ),
+    new Project(
+        "GoGoDino Fun Zone",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        true,
+        "projects/GoGoDino Fun Zone.html"
+    ),
+    new Project(
+        "WebAR Model Displayer",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.NOT_GAME,
+        false,
+        "projects/WebAR Model Displayer.html"
+    ),
+    new Project(
+        "Defender",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Defender.html"
+    ),
+    new Project(
+        "Rolling Knight",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        true,
+        "projects/Rolling Knight.html"
+    ),
+    new Project(
+        "Dive and Scavenge",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        true,
+        "projects/Dive and Scavenge.html"
+    ),
+    new Project(
+        "Into the Hole",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        true,
+        "projects/Into the Hole.html"
+    ),
+    new Project(
+        "Uncover",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Uncover.html"
+    ),
+    new Project(
+        "Rebels of the Order",
+        "You are a worker at a signal station, tasked to record data of passing ships. You will have to communicate with them using international maritime signal flags.",
+        "assets/images/Shiplogging/cover_image_630_500.png",
+        ProjectType.GAME,
+        false,
+        "projects/Rebels of the Order.html"
     )
 ]
 
